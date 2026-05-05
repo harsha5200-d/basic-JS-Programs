8dd34bac7d858c8e28a46af4b98bfa53586277f9
harsha <hreshavena@gmail.com>
2026-05-05 09:41:32 +0530

    Updated the content in sumofdigit.js

--- sumofdigit.js
+++ sumofdigit.js
@@ -2,14 +2,16 @@
 
 const prompt = require('prompt-sync')();
 
-const number = parseInt(prompt('Enter a number to find the sum of its digits: '));
-let sum = 0;
-let temp = number;
-
-while (temp>0)
-{
-    let digit = temp%10;
-    sum += digit;
-    temp = Math.floor(temp/10);
+function sumOfDigits(num) {
+    let sum = 0;
+    let temp = num;
+    while (temp > 0) {
+        let digit = temp % 10;
+        sum+=digit;
+        temp = Math.floor(temp/10);
+    }
+    return sum;
 }
-console.log('Sum of the digits: ' + sum);
+
+const num = parseInt(prompt('Enter a number to find the sum of its digits: '));
+console.log('Sum of the digits: ' + sumOfDigits(num));
